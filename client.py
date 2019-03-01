39d3cd2b8878b047324f8de18f63993e71bcc378
Zeev Rotshtein <zeevro@gmail.com>
2019-03-01 10:42:37 +0200

    Got a client and server to work!! compression broken

---
+++ client.py
@@ -0,0 +1,299 @@
+﻿from typing import Tuple, Union, Callable
+from soundz_audio import Audio, VoxAudioInputFilter, PushToTalkAudioInputFilter, AUDIO_INPUT_CALLBACK_TYPE_PROTOCOL
+import opuslib
+import threading
+import time
+import argparse
+import pynput
+import socket
+import json
+import queue
+
+
+SERVER_PORT = 4452
+SERVER_KEY = 'Rn7tEf1PKXrmHynD1QBUyluoQJDVZEbNSn7tZ0g5a8MipJEetQ'
+
+
+class UdpSocketIO(socket.socket):
+    def __init__(self, ip, port):
+        super().__init__(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        self.bind(('', 0))
+        self._addr_info = (ip, port)
+
+    def read(self):
+        return self.recv(0xFFFF)
+
+    def write(self, data):
+        return self.sendto(data, self._addr_info)
+
+
+class UdpAudioClient:
+    def __init__(self, _io, client_id: int, sample_rate: int, channels: int, samples_per_frame: int, callback: Callable[[bytes], None]):
+        self._io = _io
+
+        self._client_id = client_id
+        self._client_id_encoded = client_id.to_bytes(2, 'big')
+
+        self.sample_rate = sample_rate
+        self.channels = channels
+        self.samples_per_frame = samples_per_frame
+        self._callback = callback
+
+        self._encoder = opuslib.Encoder(sample_rate, channels, opuslib.APPLICATION_VOIP)
+        self._decoder = opuslib.Decoder(sample_rate, channels)
+
+        self._recv_thread = threading.Thread(target=self._recv_thread_func)
+        self._recv_thread.daemon = True
+        self._stop = False
+
+    @property
+    def is_alive(self):
+        return self._recv_thread.is_alive()
+
+    def _recv_thread_func(self):
+        while not self._stop:
+            try:
+                self._callback(self._read_audio_frame())
+            except Exception as e:
+                raise
+                print(f'ERROR! {e.__class__.__name__}: {e}')
+
+    def _encode_frame(self, frame: bytes) -> bytes:
+        return frame
+        return self._encoder.encode(frame, self.samples_per_frame)
+
+    def _decode_frame(self, frame: bytes) -> bytes:
+        return frame
+        return self._decoder.decode(frame, self.samples_per_frame)
+
+    def write_audio_frame(self, audio_data: bytes) -> bytes:
+        self._io.write(self._client_id_encoded + self._encode_frame(audio_data))
+        return audio_data
+
+    def _read_audio_frame(self) -> bytes:
+        return self._io.read()
+
+    def start_receiving(self):
+        self._recv_thread.start()
+
+    def __str__(self):
+        if self.channels == 1:
+            ch_str = 'Mono'
+        elif self.channels == 2:
+            ch_str = 'Stereo'
+        else:
+            ch_str = f'{self.channels} channels'
+        return f'{self.__class__.__name__}: Sapmle rate: {self.sample_rate / 1000} kHz, {ch_str}, {self.samples_per_frame} samples per frame. Backing IO is {self._io.__class__.__name__}.'
+
+
+manager_responses = {b'AudioParams': 'json',
+                     b'Auth': 'int',
+                     b'Name': '',
+                     b'Join': '',
+                     b'List': 'json',
+                     b'Leave': '',
+                     b'Start': '',
+                     b'Stop': ''}
+
+
+class TcpManagerClient:
+    def __init__(self, server_ip: str, events_callback: Callable[[bytes, int], None]=None):
+        self._server_ip = server_ip
+        self._sock = socket.socket()
+        self._sock.connect((server_ip, SERVER_PORT))
+
+        self._response_queue = queue.Queue()
+        self._event_queue = queue.Queue()
+
+        self._main_thread = threading.Thread(target=self._main_loop)
+        self._main_thread.daemon = True
+        self._main_thread.start()
+
+        self._events_callback = events_callback
+        if events_callback:
+            self._events_thread = threading.Thread(target=self._event_loop)
+            self._events_thread.daemon = True
+            self._event_thread.start()
+
+    def _decode_payload(self, decoder: str, payload: bytes) -> Union[None, str, dict, list, int]:
+        if not payload:
+            return
+
+        if decoder == 'json':
+            return json.loads(payload.decode())
+
+        if decoder == 'int':
+            return int.from_bytes(payload, 'big')
+
+        return payload.decode()
+
+    def _read_frame(self):
+        frame_size = int.from_bytes(self._sock.recv(3), 'big')  # Frame size
+        if not frame_size:
+            return None, None
+        frame = self._sock.recv(frame_size)                     # Frame data
+        command, payload = frame[1:frame[0] + 1], frame[frame[0] + 1:]
+
+        is_response = True
+        if command in manager_responses:
+            decoder = manager_responses[command]
+        elif command.endswith(b'Ok') and command[:-2] in manager_responses:
+            decoder = manager_responses[command[:-2]]
+        elif command.startswith(b'Bad') and command[3:] in manager_responses:
+            decoder = manager_responses[command[3:]]
+        else:
+            is_response = False
+            decoder = 'int'
+
+        payload = self._decode_payload(decoder, payload)
+
+        (self._response_queue if is_response else self._event_queue).put((command, payload))
+
+        #print('<--', (command, payload))
+
+    def _main_loop(self) -> None:
+        while 1:
+            self._read_frame()
+
+    def _event_loop(self) -> None:
+        while 1:
+            try:
+                self._events_callback(*self.get_event())
+            except Exception:
+                pass
+
+    def _write_frame(self, command: bytes, payload: Union[bytes, str]=None) -> None:
+        if payload is None:
+            payload = b''
+        elif isinstance(payload, str):
+            payload = payload.encode()
+        self._sock.send((1 + len(command) + len(payload)).to_bytes(3, 'big'))  # Frame size
+        self._sock.send(len(command).to_bytes(1, 'big') + command + payload)   # Frame data
+        #print('-->', (command, payload))
+
+    def _read_response(self) -> Tuple[bytes, Union[None, dict, list, int]]:
+        return self._response_queue.get()
+
+    def request(self, command: bytes, payload: str=None) -> Tuple[bytes, Union[None, dict, list, int]]:
+        self._write_frame(command, payload)
+        return self._read_response()
+
+    def get_event(self, block=True, timeout=None) -> Tuple[bytes, int]:
+        return self._event_queue.get(block, timeout)
+
+
+def get_ptt_key():
+    print('Press the key you wish to use for PTT. Press ESC to cancel.')
+
+    key_l = []
+
+    def on_press(key):
+        print(f'on_press({key})')
+        key_l.append(key)
+        return False
+
+    keyboard_listener = pynput.keyboard.Listener(on_press=on_press, suppress=True)
+    keyboard_listener.start()
+    keyboard_listener.join()
+
+    selected_key, = key_l
+
+    if selected_key == pynput.keyboard.Key.esc:
+        print('Cancelled.')
+        return None
+
+    print(f'{selected_key} was selected.')
+
+    return selected_key
+
+
+def tx_status_callback(frame):
+    print('TX' if frame else '  ', end='\r')
+    return frame
+
+
+def main():
+    p = argparse.ArgumentParser()
+    p.add_argument('-p', '--push-to-talk', action='store_true')
+    p.add_argument('-P', '--server-port', type=int, default=SERVER_PORT)
+    p.add_argument('-s', '--server-ip', default='127.0.0.1')
+    p.add_argument('-k', '--server-key', default=SERVER_KEY)
+    p.add_argument('-n', '--name', required=True)
+    args = p.parse_args()
+
+    print('Hello')
+    print()
+
+    ptt_key = None
+    if args.push_to_talk:
+        ptt_key = get_ptt_key()
+
+    print(f'Using {"PTT" if ptt_key is not None else "Vox"} filter.')
+
+    manager_client = TcpManagerClient(args.server_ip)
+
+    print('Getting audio parameters from server...', end='')
+    audio_params = manager_client.request(b'GetAudioParams')[1]
+    print(' OK')
+
+    print('Authenticating to server...', end='')
+    command, payload = manager_client.request(b'Auth', SERVER_KEY)
+    if command != b'AuthOk':
+        print(' FAIL')
+        print(f'Authentication process failed! {payload}')
+        return
+    client_id = payload
+    print(' OK')
+    print(f'My client ID is {client_id}')
+
+    print('Initialising audio system...', end='')
+    audio = Audio(input_needed=True, output_needed=True, **audio_params)
+    network_stream = UdpAudioClient(UdpSocketIO(args.server_ip, args.server_port), client_id, callback=audio.playback, **audio_params)
+    audio.add_callback(network_stream.write_audio_frame)
+    _tx_filter = VoxAudioInputFilter(audio) if ptt_key is None else PushToTalkAudioInputFilter(audio, ptt_key)
+    #audio.add_callback(tx_status_callback, AUDIO_INPUT_CALLBACK_TYPE_PROTOCOL)
+    print(' OK')
+
+    print('Setting your name...', end='')
+    command, payload = manager_client.request(b'SetName', args.name)
+    if command != b'NameOk':
+        print(' FAIL')
+        print(f'Setting name failed! {payload}')
+        return
+    print(' OK')
+
+    print('Joining voice channel...', end='')
+    command, payload = manager_client.request(b'JoinChannel')
+    if command != b'JoinOk':
+        print(' FAIL')
+        print(f'Join failed! {payload}')
+        return
+    print(' OK')
+
+    print('Listing users in channel...', end='')
+    command, payload = manager_client.request(b'ListChannelUsers')
+    if command != b'ListOk':
+        print(' FAIL')
+        print(f'Listing failed! {payload}')
+        return
+    print(' OK')
+
+    print()
+    print('User list:')
+    for user in payload:
+        print(user)
+
+    print()
+    print('Start.')
+    audio.start_capture()
+    network_stream.start_receiving()
+
+    try:
+        while 1:
+            time.sleep(10)
+    except (KeyboardInterrupt, SystemExit):
+        print('Stop.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ server.py
@@ -0,0 +1,265 @@
+﻿from typing import Tuple, Iterable
+from socketserver import ThreadingTCPServer, StreamRequestHandler
+import threading
+import socket
+import json
+import time
+
+
+SERVER_PORT = 4452
+
+AUDIO_PARAMS = {'sample_rate': 12000, 'channels': 1, 'samples_per_frame': 120}
+
+
+secret_key = 'Rn7tEf1PKXrmHynD1QBUyluoQJDVZEbNSn7tZ0g5a8MipJEetQ'
+
+
+def udp_audio_loop():
+    rx_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    rx_sock.bind(('0.0.0.0', SERVER_PORT))
+
+    tx_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+    while 1:
+        frame, rx_addr_info = rx_sock.recvfrom(1024)
+        client_id, audio_data = int.from_bytes(frame[:2], 'big'), frame[2:]
+
+        for tx_addr_info in client_manager.audio_received(client_id, rx_addr_info):
+            try:
+                tx_sock.sendto(audio_data, tx_addr_info)
+            except Exception:
+                pass
+
+
+class ClientManagerRequestHandler(StreamRequestHandler):
+    def setup(self):
+        self._client_id = None
+
+    def handle(self):
+        while 1:
+            try:
+                command, payload = self._read_frame()
+            except ConnectionError:
+                break
+            except Exception:
+                self.write_frame(b'BadFrame')
+                continue
+
+            if command is None:
+                break
+
+            command = command.decode()
+            handler = getattr(self, f'handle_{command}', None)
+            if handler is None:
+                self.write_frame(b'BadCommand', command)
+                continue
+
+            self.write_frame(*handler(payload))
+
+    def finish(self):
+        if self._client_id is not None:
+            client_manager.remove(self._client_id)
+
+    def _read_frame(self) -> Tuple[bytes, bytes]:
+        frame_size = int.from_bytes(self.request.recv(3), 'big')  # Frame size
+        if not frame_size:
+            return None, None
+        frame = self.request.recv(frame_size)                     # Frame data
+        command, payload_bytes = frame[1:frame[0] + 1], frame[frame[0] + 1:]
+        print('<--', command, payload_bytes)
+        return command, payload_bytes.decode() if payload_bytes else None
+
+    def write_frame(self, command: bytes, payload: bytes=None) -> None:
+        if payload is None:
+            payload = b''
+        elif isinstance(payload, str):
+            payload = payload.encode()
+        self.request.send((1 + len(command) + len(payload)).to_bytes(3, 'big'))  # Frame size
+        print('-->', (command, payload))
+        self.request.send(len(command).to_bytes(1, 'big') + command + payload)   # Frame data
+
+    def handle_GetAudioParams(self, payload: str) -> Tuple[bytes, str]:
+        return (b'AudioParams', json.dumps(AUDIO_PARAMS, separators=(',', ':')))
+
+    def handle_Auth(self, payload: str) -> Tuple[bytes, str]:
+        if payload != secret_key:
+            success, payload = False, 'Wrong key'
+        else:
+            success, payload = client_manager.new(self)
+            self._client_id = int.from_bytes(payload, 'big')
+        return (b'AuthOk' if success else b'BadAuth', payload)
+
+    def handle_SetName(self, payload: str) -> Tuple[bytes, str]:
+        if self._client_id is None:
+            success, payload = False, 'Not authenticated'
+        else:
+            success, payload = client_manager.set_name(self._client_id, payload)
+        return (b'NameOk' if success else b'BadName', payload)
+
+    def handle_JoinChannel(self, payload: str) -> Tuple[bytes, str]:
+        if self._client_id is None:
+            success, payload = False, 'Not authenticated'
+        else:
+            success, payload = client_manager.join_channel(self._client_id, payload)
+        return (b'JoinOk' if success else b'BadJoin', payload)
+
+    def handle_ListChannelUsers(self, payload: str):
+        if self._client_id is None:
+            success, payload = False, 'Not authenticated'
+        else:
+            success, payload = client_manager.list_channel_users(self._client_id, payload)
+        return (b'ListOk' if success else b'BadList', payload)
+
+    def handle_LeaveChannel(self, payload: str) -> Tuple[bytes, str]:
+        if self._client_id is None:
+            success, payload = False, 'Not authenticated'
+        else:
+            success, payload = client_manager.leave_channel(self._client_id)
+        return (b'LeaveOk' if success else b'BadLeave', payload)
+
+    def handle_StartTalking(self, payload: str) -> Tuple[bytes, str]:
+        if self._client_id is None:
+            success, payload = False, 'Not authenticated'
+        else:
+            success, payload = client_manager.client_started_talking(self._client_id)
+        return (b'StartOk' if success else b'BadStart', payload)
+
+    def handle_StopTalking(self, payload: str) -> Tuple[bytes, str]:
+        if self._client_id is None:
+            success, payload = False, 'Not authenticated'
+        else:
+            success, payload = client_manager.client_stopped_talking(self._client_id)
+        return (b'StopOk' if success else b'BadStop', payload)
+
+
+class Client:
+    def __init__(self, handler):
+        self.handler = handler
+        self.name = None
+        self.in_channel = False
+        self.udp_addr_info = None
+
+
+class ClientManager:
+    def __init__(self):
+        self._clients = {}
+
+    def _broadcast(self, source_client_id: int, command: bytes, extra_payload: str=None):
+        if source_client_id is None:
+            for c in self._clients:
+                c.handler.write_frame(command, extra_payload)
+        else:
+            payload = source_client_id.to_bytes(2, 'big') + (extra_payload.encode() if extra_payload else b'')
+            for cid, c in self._clients.items():
+                if cid == source_client_id:
+                    continue
+                c.handler.write_frame(command, payload)
+
+    @property
+    def taken_ids(self):
+        return (cid for cid in self._clients)
+
+    @property
+    def taken_names(self):
+        return (c.name for c in self._clients.values())
+
+    @property
+    def channel_user_list(self):
+        return ((cid, c.name) for cid, c in self._clients.items() if c.in_channel)
+
+    def new(self, handler: StreamRequestHandler) -> Tuple[bool, int]:
+        if len(self._clients) >= 0xFFFF:
+            return False, 'Server is full'
+        if not self._clients:
+            new_id = 0
+        else:
+            for new_id, i in enumerate(sorted(self.taken_ids)):
+                if new_id != i:
+                    break
+            else:
+                new_id += 1
+        self._clients[new_id] = Client(handler)
+        return True, new_id.to_bytes(2, 'big')
+
+    def remove(self, client_id: int) -> Tuple[bool, str]:
+        if self._clients.pop(client_id, None) is None:
+            return False, 'Nonexistent client id'
+        return True, None
+
+    def set_name(self, client_id: int, name: str) -> Tuple[bool, str]:
+        c = self._clients[client_id]
+        if not name:
+            return False, 'Empty name'
+        if name == c.name:
+            return False, 'Same name'
+        if name in self.taken_names:
+            return False, 'Already taken'
+        self._clients[client_id].name = name
+        self._broadcast(client_id, b'Name', name)
+        return True, None
+
+    def list_channel_users(self, client_id: int, channel: str) -> Tuple[bool, str]:
+        return True, json.dumps(list(self.channel_user_list), separators=(',', ':'))
+
+    def join_channel(self, client_id: int, channel: None) -> Tuple[bool, str]:
+        c = self._clients[client_id]
+        if not c.name:
+            return False, 'Empty name'
+        if c.in_channel:
+            return False, 'Already in channel'
+        c.in_channel = True
+        self._broadcast(client_id, b'JoinedChannel')
+        return True, None
+
+    def leave_channel(self, client_id: int) -> Tuple[bool, str]:
+        c = self._clients[client_id]
+        if not c.in_channel:
+            return False, 'Not in channel'
+        c.in_channel = False
+        self._broadcast(client_id, b'LeftChannel')
+        return True, None
+
+    def client_started_talking(self, client_id: int) -> Tuple[bool, str]:
+        if not self._clients[client_id].in_channel:
+            return False, 'Not in channel'
+        self._broadcast(client_id, b'StartedTalking')
+        return True, None
+
+    def client_stopped_talking(self, client_id: int) -> Tuple[bool, str]:
+        if not self._clients[client_id].in_channel:
+            return False, 'Not in channel'
+        self._broadcast(client_id, b'StoppedTalking')
+        return True, None
+
+    def audio_received(self, client_id: int, addr_info: Tuple[str, int]) -> Iterable[Tuple[str, int]]:
+        for cid, c in self._clients.items():
+            if cid == client_id:
+                c.udp_addr_info = addr_info
+                continue
+            yield c.udp_addr_info
+
+
+client_manager = ClientManager()
+
+
+def main():
+    client_manager = ThreadingTCPServer(('0.0.0.0', SERVER_PORT), ClientManagerRequestHandler)
+    client_manager_thread = threading.Thread(target=client_manager.serve_forever)
+    client_manager_thread.daemon = True
+
+    audio_thread = threading.Thread(target=udp_audio_loop)
+    audio_thread.daemon = True
+
+    client_manager_thread.start()
+    audio_thread.start()
+
+    print('Server started!')
+    try:
+        while 1:
+            time.sleep(10)
+    except (KeyboardInterrupt, SystemExit):
+        print('Stop')
+
+
+if __name__ == '__main__':
+    main()
