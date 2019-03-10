334cdb5e5a21b01180111000f46826c607352b9f
Zeev Rotshtein <zeev@vdoo.com>
2019-03-10 19:27:47 +0200

    server.py: No more global variables

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 ﻿from typing import Tuple, Iterable
 from socketserver import ThreadingTCPServer, StreamRequestHandler
-from collections import deque
 import threading
 import socket
 import json
@@ -18,114 +17,6 @@ AUDIO_PARAMS = {'sample_rate': FRAME_RATE, 'channels': CHANNELS, 'samples_per_fr
 
 
 secret_key = 'Rn7tEf1PKXrmHynD1QBUyluoQJDVZEbNSn7tZ0g5a8MipJEetQ'
-
-
-def udp_audio_loop():
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('0.0.0.0', SERVER_PORT))
-
-    tx_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-    tx_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    tx_sock.bind(('0.0.0.0', SERVER_PORT))
-
-    while 1:
-        frame, rx_addr_info = sock.recvfrom(1024)
-        client_id = int.from_bytes(frame[:2], 'big')
-
-        for tx_addr_info in client_manager.audio_received(client_id, rx_addr_info):
-            try:
-                tx_sock.sendto(frame, tx_addr_info)
-            except Exception:
-                pass
-
-
-class ClientManagerRequestHandler(StreamRequestHandler):
-    def setup(self):
-        super().setup()
-        self._client_id = None
-
-    def handle(self):
-        while 1:
-            try:
-                command, payload = self._read_frame()
-            except ConnectionError:
-                break
-            except Exception:
-                self.write_frame(b'BadFrame')
-                continue
-
-            if command is None:
-                break
-
-            command = command.decode()
-            handler = getattr(self, f'handle_{command}', None)
-            if handler is None:
-                self.write_frame(b'BadCommand', command)
-                continue
-
-            self.write_frame(*handler(payload))
-
-    def finish(self):
-        if self._client_id is not None:
-            client_manager.remove(self._client_id)
-
-    def _read_frame(self) -> Tuple[bytes, bytes]:
-        frame_size = int.from_bytes(self.request.recv(3), 'big')  # Frame size
-        if not frame_size:
-            return None, None
-        frame = self.request.recv(frame_size)                     # Frame data
-        command, payload_bytes = frame[1:frame[0] + 1], frame[frame[0] + 1:]
-        print(f'{"???" if self._client_id is None else self._client_id:3} <-- {command.decode()} {payload_bytes}')
-        return command, payload_bytes.decode() if payload_bytes else None
-
-    def write_frame(self, command: bytes, payload: bytes=None) -> None:
-        if payload is None:
-            payload = b''
-        elif isinstance(payload, str):
-            payload = payload.encode()
-        self.request.send((1 + len(command) + len(payload)).to_bytes(3, 'big'))  # Frame size
-        print(f'{"???" if self._client_id is None else self._client_id:3} --> {command.decode()} {payload}')
-        self.request.send(len(command).to_bytes(1, 'big') + command + payload)   # Frame data
-
-    def handle_GetAudioParams(self, payload: str) -> Tuple[bytes, str]:
-        return (b'AudioParams', json.dumps(AUDIO_PARAMS, separators=(',', ':')))
-
-    def handle_Auth(self, payload: str) -> Tuple[bytes, str]:
-        if payload != secret_key:
-            success, payload = False, 'Wrong key'
-        else:
-            success, payload = client_manager.new(self)
-            self._client_id = int.from_bytes(payload, 'big')
-        return (b'AuthOk' if success else b'BadAuth', payload)
-
-    def handle_SetName(self, payload: str) -> Tuple[bytes, str]:
-        if self._client_id is None:
-            success, payload = False, 'Not authenticated'
-        else:
-            success, payload = client_manager.set_name(self._client_id, payload)
-        return (b'NameOk' if success else b'BadName', payload)
-
-    def handle_JoinChannel(self, payload: str) -> Tuple[bytes, str]:
-        if self._client_id is None:
-            success, payload = False, 'Not authenticated'
-        else:
-            success, payload = client_manager.join_channel(self._client_id, payload)
-        return (b'JoinOk' if success else b'BadJoin', payload)
-
-    def handle_ListChannelUsers(self, payload: str) -> Tuple[bytes, str]:
-        if self._client_id is None:
-            success, payload = False, 'Not authenticated'
-        else:
-            success, payload = client_manager.list_channel_users(self._client_id, payload)
-        return (b'ListOk' if success else b'BadList', payload)
-
-    def handle_LeaveChannel(self, payload: str) -> Tuple[bytes, str]:
-        if self._client_id is None:
-            success, payload = False, 'Not authenticated'
-        else:
-            success, payload = client_manager.leave_channel(self._client_id)
-        return (b'LeaveOk' if success else b'BadLeave', payload)
 
 
 class Client:
@@ -146,7 +37,7 @@ class ClientManager:
     def __init__(self):
         self._clients = {}
 
-    def _broadcast(self, source_client_id: int, command: bytes, extra_payload: str=None):
+    def _broadcast(self, source_client_id: int, command: bytes, extra_payload: str = None):
         if source_client_id is None:
             for c in self._clients:
                 c.handler.write_frame(command, extra_payload)
@@ -201,10 +92,10 @@ class ClientManager:
         self._broadcast(client_id, b'Name', name)
         return True, None
 
-    def list_channel_users(self, client_id: int, channel: str) -> Tuple[bool, str]:
+    def list_channel_users(self, client_id: int, channel: str) -> Tuple[bool, str]:  #pylint: disable=unused-argument
         return True, json.dumps(list(self.channel_user_list), separators=(',', ':'))
 
-    def join_channel(self, client_id: int, channel: None) -> Tuple[bool, str]:
+    def join_channel(self, client_id: int, channel: None) -> Tuple[bool, str]:  #pylint: disable=unused-argument
         c = self._clients[client_id]
         if not c.name:
             return False, 'Empty name'
@@ -231,24 +122,139 @@ class ClientManager:
             yield c.udp_addr_info
 
 
-client_manager = ClientManager()
+class ClientManagerRequestHandler(StreamRequestHandler):
+    def setup(self):
+        super().setup()
+        self._client_id = None
+
+    def handle(self):
+        while 1:
+            try:
+                command, payload = self._read_frame()
+            except ConnectionError:
+                break
+            except Exception:
+                self.write_frame(b'BadFrame')
+                continue
+
+            if command is None:
+                break
+
+            command = command.decode()
+            handler = getattr(self, f'handle_{command}', None)
+            if handler is None:
+                self.write_frame(b'BadCommand', command)
+                continue
+
+            self.write_frame(*handler(payload))
+
+    def finish(self):
+        if self._client_id is not None:
+            self.server.client_manager.remove(self._client_id)
+
+    def _read_frame(self) -> Tuple[bytes, bytes]:
+        frame_size = int.from_bytes(self.request.recv(3), 'big')  # Frame size
+        if not frame_size:
+            return None, None
+        frame = self.request.recv(frame_size)                     # Frame data
+        command, payload_bytes = frame[1:frame[0] + 1], frame[frame[0] + 1:]
+        print(f'{"???" if self._client_id is None else self._client_id:3} <-- {command.decode()} {payload_bytes}')
+        return command, payload_bytes.decode() if payload_bytes else None
+
+    def write_frame(self, command: bytes, payload: bytes = None) -> None:
+        if payload is None:
+            payload = b''
+        elif isinstance(payload, str):
+            payload = payload.encode()
+        self.request.send((1 + len(command) + len(payload)).to_bytes(3, 'big'))  # Frame size
+        print(f'{"???" if self._client_id is None else self._client_id:3} --> {command.decode()} {payload}')
+        self.request.send(len(command).to_bytes(1, 'big') + command + payload)   # Frame data
+
+    def handle_GetAudioParams(self, payload: str) -> Tuple[bytes, str]:  #pylint: disable=unused-argument
+        return (b'AudioParams', json.dumps(AUDIO_PARAMS, separators=(',', ':')))
+
+    def handle_Auth(self, payload: str) -> Tuple[bytes, str]:
+        if payload != secret_key:
+            success, payload = False, 'Wrong key'
+        else:
+            success, payload = self.server.client_manager.new(self)
+            self._client_id = int.from_bytes(payload, 'big')
+        return (b'AuthOk' if success else b'BadAuth', payload)
+
+    def handle_SetName(self, payload: str) -> Tuple[bytes, str]:
+        if self._client_id is None:
+            success, payload = False, 'Not authenticated'
+        else:
+            success, payload = self.server.client_manager.set_name(self._client_id, payload)
+        return (b'NameOk' if success else b'BadName', payload)
+
+    def handle_JoinChannel(self, payload: str) -> Tuple[bytes, str]:
+        if self._client_id is None:
+            success, payload = False, 'Not authenticated'
+        else:
+            success, payload = self.server.client_manager.join_channel(self._client_id, payload)
+        return (b'JoinOk' if success else b'BadJoin', payload)
+
+    def handle_ListChannelUsers(self, payload: str) -> Tuple[bytes, str]:
+        if self._client_id is None:
+            success, payload = False, 'Not authenticated'
+        else:
+            success, payload = self.server.client_manager.list_channel_users(self._client_id, payload)
+        return (b'ListOk' if success else b'BadList', payload)
+
+    def handle_LeaveChannel(self, payload: str) -> Tuple[bytes, str]:
+        if self._client_id is None:
+            success, payload = False, 'Not authenticated'
+        else:
+            success, payload = self.server.client_manager.leave_channel(self._client_id)
+        return (b'LeaveOk' if success else b'BadLeave', payload)
+
+
+class TCPServer(ThreadingTCPServer):
+    def __init__(self, client_manager: ClientManager):
+        super().__init__(('0.0.0.0', SERVER_PORT), ClientManagerRequestHandler)
+        self.client_manager = client_manager
+
+
+class UDPServer:
+    def __init__(self, client_manager: ClientManager):
+        self.client_manager = client_manager
+
+    def serve_forever(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(('0.0.0.0', SERVER_PORT))
+
+        tx_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+        tx_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        tx_sock.bind(('0.0.0.0', SERVER_PORT))
+
+        while 1:
+            frame, rx_addr_info = sock.recvfrom(1024)
+            client_id = int.from_bytes(frame[:2], 'big')
+
+            for tx_addr_info in self.client_manager.audio_received(client_id, rx_addr_info):
+                try:
+                    tx_sock.sendto(frame, tx_addr_info)
+                except Exception as e:
+                    print(f'ERROR in udp_audio_loop! {e.__class__.__name__}: {e}')
 
 
 def main():
-    client_manager = ThreadingTCPServer(('0.0.0.0', SERVER_PORT), ClientManagerRequestHandler)
-    client_manager_thread = threading.Thread(target=client_manager.serve_forever)
-    client_manager_thread.daemon = True
+    client_manager = ClientManager()
 
-    audio_thread = threading.Thread(target=udp_audio_loop)
-    audio_thread.daemon = True
+    tcp_server = TCPServer(client_manager)
+    udp_server = UDPServer(client_manager)
 
-    client_manager_thread.start()
-    audio_thread.start()
+    server_threads = [threading.Thread(target=server.serve_forever) for server in (tcp_server, udp_server)]
+    for thread in server_threads:
+        thread.daemon = True
+        thread.start()
 
     print('Server started!')
     try:
-        while 1:
-            time.sleep(10)
+        while all(thread.is_alive() for thread in server_threads):
+            time.sleep(0.5)
     except (KeyboardInterrupt, SystemExit):
         print('Stop')
 
